5a2a60a66b1fd99ee6d447f2faf3eb2eb09fddc5
Carlos Freitas <casc2@cin.ufpe.br>
2019-11-02 03:29:12 -0300

    Implementing more state machines

--- Deadline 4/DecoderEncoder.c
+++ Deadline 4/DecoderEncoder.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /********** Interframe Space ***********/
 #define INTERFRAME_SPACE                0
@@ -52,26 +53,30 @@
 
 #define MAX_FRAME_SIZE 127
 
-unsigned char currentFrameField     = INTERFRAME_SPACE;
-unsigned char currentFrameSubField  = INTERFRAME_SPACE_BUS_IDLE;
+unsigned char currentFrameField    = INTERFRAME_SPACE;
+unsigned char currentFrameSubField = INTERFRAME_SPACE_BUS_IDLE;
 
 unsigned char frameBuf[MAX_FRAME_SIZE];
-unsigned char bitIndex      = 0;
-unsigned char sampledBit    = 0;
+unsigned char sampledBit;
+unsigned char bitIndex = 0;
+unsigned char bitCnt   = 0;
+
+unsigned char RTR;
+unsigned char SRR;
+unsigned char IDE;
+unsigned char DLC;
 
 void decoderStateMachine();
 
 void interframeSpaceStateMachine() {
+    printf("Interframe space\n");
     switch(currentFrameSubField) {
         case INTERFRAME_SPACE_INTERMISSION:
             break;
         case INTERFRAME_SPACE_BUS_IDLE:
             if (sampledBit == '0') {
-                printf("INTERFRAME_SPACE_BUS_IDLE -> START_OF_FRAME\n");
                 currentFrameField = START_OF_FRAME;
                 decoderStateMachine();
-            } else if(sampledBit == '1') {
-                printf("INTERFRAME_SPACE_BUS_IDLE\n");
             }
             break;
         default:
@@ -81,13 +86,96 @@ void interframeSpaceStateMachine() {
 };
 
 void startOfFrameStateMachine() {
-    printf("START_OF_FRAME\n");
-    bitIndex = 0;
-    frameBuf[bitIndex++] = sampledBit;
+    printf("Start of Frame\n");
     // TODO: Trigger hard synchronisation.
+    bitIndex = 0;
+    IDE = 0;  // Assuming Standard format.
+    frameBuf[bitIndex++] = sampledBit;
     currentFrameField = ARBITRATION;
-    printf("%c", frameBuf[0]);
+    currentFrameSubField = ARBITRATION_IDENTIFIER_11_BIT;
 };
+
+void arbitrationStateMachine() {
+    printf("Arbitration\n");
+    switch (currentFrameSubField) {
+        case ARBITRATION_IDENTIFIER_11_BIT:
+            frameBuf[bitIndex++] = sampledBit;
+            if (bitIndex == 12) currentFrameSubField = ARBITRATION_RTR; // Assuming Standard format.
+            break;
+        case ARBITRATION_RTR:
+            RTR = sampledBit;
+            frameBuf[bitIndex++] = sampledBit;
+            currentFrameField = CONTROL;
+            if (IDE == '1') {
+                currentFrameSubField = CONTROL_r1;
+            } else {
+                currentFrameSubField = CONTROL_IDE;
+            }
+            break;
+        case ARBITRATION_SRR: // Empty transition to IDE bit.
+            SRR = RTR;
+            currentFrameSubField = ARBITRATION_IDE;
+            arbitrationStateMachine();
+            break;
+        case ARBITRATION_IDE: // Empty transition to 18 bit identifier.
+            currentFrameSubField = ARBITRATION_IDENTIFIER_18_BIT;
+            break;
+        case ARBITRATION_IDENTIFIER_18_BIT:
+            frameBuf[bitIndex++] = sampledBit;
+            if (bitIndex == 32) currentFrameSubField = ARBITRATION_RTR; // Assuming Standard format.
+            break;
+        default:
+            printf("Arbitration error: invalid sub-frame field.\n");
+            break;
+    }
+}
+
+void controlStateMachine() {
+    printf("Control\n");
+    switch (currentFrameSubField) {
+        case CONTROL_IDE:
+            IDE = sampledBit;
+            frameBuf[bitIndex++] = sampledBit;
+            if (IDE == '0') { // Standard format.
+                currentFrameSubField = CONTROL_r0;
+            } else { // Extended format.
+                currentFrameField = ARBITRATION;
+                currentFrameSubField = ARBITRATION_SRR;
+                arbitrationStateMachine();
+            }
+            break;
+        case CONTROL_r1:
+            frameBuf[bitIndex++] = sampledBit;
+            currentFrameSubField = CONTROL_r0;
+            break;
+        case CONTROL_r0:
+            frameBuf[bitIndex++] = sampledBit;
+            currentFrameSubField = CONTROL_DLC;
+            bitCnt = 4;
+            break;
+        case CONTROL_DLC:
+            frameBuf[bitIndex++] = sampledBit;
+            bitCnt--;
+            DLC += ((sampledBit - '0') << bitCnt);
+            if (bitCnt == 0) {
+                DLC = fmin(DLC, 8); // Maximum number of data bytes: 8.
+                printf("%d\n", DLC);
+                if (RTR == '0') currentFrameField = DATA; // Data frame.
+                else currentFrameField = CRC; // Remote frame.
+            }
+            break;
+        default:
+            printf("Control error: invalid sub-frame field.\n");
+            break;
+    }
+}
+
+void dataStateMachine() {
+    printf("Data\n");
+    frameBuf[bitIndex++] = sampledBit;
+    bitCnt++;
+    if (bitCnt == 8 * DLC) currentFrameField = CRC;
+}
 
 void decoderStateMachine() {
     switch(currentFrameField) {
@@ -98,10 +186,13 @@ void decoderStateMachine() {
             startOfFrameStateMachine();
             break;
         case ARBITRATION:
+            arbitrationStateMachine();
             break;
         case CONTROL:
+            controlStateMachine();
             break;
         case DATA:
+            dataStateMachine();
             break;
         case CRC:
             break;
